7a1bd7a124fe33e5ffbce0ed4eda61c5e9c5ad64
taroimo1492 <frogfrog1492@gmail.com>
2015-08-30 01:12:29 +0900

    Xcodeのプロジェクトをコミットします

---
+++ Kaihatsu_Ishin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaihatsu_Ishin
+//
+//  Created by taroimo on 2015/08/30.
+//  Copyright (c) 2015年 taroimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Kaihatsu_IshinTests/Kaihatsu_IshinTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kaihatsu_IshinTests.swift
+//  Kaihatsu_IshinTests
+//
+//  Created by taroimo on 2015/08/30.
+//  Copyright (c) 2015年 taroimo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Kaihatsu_IshinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
